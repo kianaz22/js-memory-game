596dffb2b5231241821bc9fef1d7a78c1d2f9462
unknown <kiana.zrb@gmail.com>
2021-11-02 13:32:46 +0330

    Updates

---
+++ sw.js
@@ -0,0 +1,19 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+
+workbox.precaching.precacheAndRoute([
+    {"revision":"10306321792f10fea662c5a2328a7dff","url":"build/img/10C.jpg"},
+    {"revision":"2aa6787199044aa32c26051de37701e8","url":"build/img/2D.jpg"},
+    {"revision":"8fccba55ad0185cae67fbf191a92121a","url":"build/img/4H.jpg"},
+    {"revision":"6d26f2e140fa9262b6fefe18cdd798d6","url":"build/img/5S.jpg"},
+    {"revision":"25680783dfa2cd51989ed1c519a8904f","url":"build/img/7H.jpg"},
+    {"revision":"46765e088045b1209903fee664b6a5a0","url":"build/img/AD.jpg"},
+    {"revision":"d259878f946c6ac00d827521fb495ca0","url":"build/img/AS.jpg"},
+    {"revision":"92c3486809610d773aa41f6763319be1","url":"build/img/bg2.jpg"},
+    {"revision":"278425b19a846387e593cfafe72c0a01","url":"build/img/cardback11.jpg"},
+    {"revision":"4cdd8ae07e2e18eb775f1997c5e57146","url":"build/img/JC.jpg"},
+    {"revision":"eac290f7e7ec1d519b17d8c524f1e300","url":"build/img/KH.jpg"},
+    {"revision":"33f58f4b815a6410a74be43f888a8fe8","url":"build/img/QS.jpg"},
+    {"revision":"f689e3237709d5ad69cd762a18c13e9e","url":"build/index.html"},
+    {"revision":"cdc610acad1b6046979e02b2abd5e642","url":"build/script.js"},
+    {"revision":"d709a1df597f342552ebab16d8515bd0","url":"build/style.css"},
+    {"revision":"7c2b709274035146d79e08f781371b22","url":"workbox-077d92df.js"}])
